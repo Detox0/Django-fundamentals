1553dbae8e6b3cf406afb5a6fb630d8ade93b7e6
Sebastián Vallejos <vallejos.sa@gmail.com>
2017-10-09 14:33:48 -0300

    Created a template view, where you can display different types of treasures, then a model called Treasure was created, still not implemented nor migrated.

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,21 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
 
-    return HttpResponse('<h1>Hello Explorers!<h1>')
+    return render(request, 'index.html', {'treasures' : treasures})
+
+class Treasure:
+    def __init__(self, name, value, material, location):
+        self.name = name
+        self.value = value
+        self.material = material
+        self.location = location
+
+
+treasures = [
+    Treasure('Gold Nugget', 500.00, 'gold',"Curly's Creek, NM"),
+    Treasure("Fool's Gold", 0.00, 'pyrite',"Fool's Falls,CO"),
+    Treasure('Coffe Can', 200.00, 'tin',"Acme, CA"),
+]
